d5c5fbbdf8705b26fa5098d447796aaa518160ed
antony-guthiga <toniguthiga93@gmail.com>
2020-06-18 08:18:51 +0300

    Add new features

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Goal } from '../goal';
+import {  ActivatedRoute, ParamMap } from '@angular/router';
+import { GoalService } from '../goal-service/goal.service';
 
 @Component({
   selector: 'app-goal-detail',
@@ -7,14 +9,13 @@ import { Goal } from '../goal';
   styleUrls: ['./goal-detail.component.css']
 })
 export class GoalDetailComponent implements OnInit {
-  @Input() goal: Goal;
-  @Output() isComplete = new EventEmitter<boolean>();
+  goal:Goal;
 
-  goalDelete(complete:boolean){
-    this.isComplete.emit(complete);
+  constructor(private route:ActivatedRoute, private service:GoalService) { }
+
+  ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    this.goal = this.service.getGoal(id)
   }
-  constructor() { }
-
-  ngOnInit(){}
 
 }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
+import { HttpClient } from '@angular/common/http';
+import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-goal',
@@ -8,23 +14,26 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
 
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,9,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,11,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,3,14)),
-  ];
+  goals:Goal[];
+  alertService:AlertService;
+  quote:Quote;
 
-  deleteGoal(isComplete, index){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+  goToUrl(id){
+    this.router.navigate(['/goals',id])
+  }
 
-      if (toDelete){
-        this.goals.splice(index,1)
-      }
+  deleteGoal(index){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("Goal has been deleted")
     }
+  }
+
+  constructor(goalService:GoalService, alertService:AlertService, private quoteService:QuoteRequestService, private router:Router) {
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
   }
 
   addNewGoal(goal){
@@ -44,8 +53,8 @@ export class GoalComponent implements OnInit {
     }
   }
 
-  constructor() { }
-
-  ngOnInit(){}
-
+  ngOnInit(){
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
+  }
 }
